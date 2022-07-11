b41d414a840b32511e15fbebffb5d33192fc9eb9
trajectory100 <[farzanehshams43@gmail.com]>
2022-07-11 15:09:55 +0200

    stil working on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,29 @@ import Logo1 from './Logo/Logo1/LanthanLogo.jpg';
 import Logo2 from './Logo/Logo2/Lanthan-Safe-Sky_Link.png';
 // import Navigation from './components/Navigation/Navigation';
 
-
- 
 const Header = () => {
   return (
           <div>
             <header>
               <div className={"logobox"}>
-                <span className={"logobox.item1"}><img alt="logo1"  src={Logo1}  /></span>  
-                <span className={"logobox.item2"}><img alt="logo2"  src={Logo2}  /></span> 
-                <span className={"logobox.item3"}>Hello</span>                     
+                <span className={"item1"}><img alt="logo1"  src={Logo1}  /></span>  
+                <span className={"item2"}><img alt="logo2"  src={Logo2}  /></span> 
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+                <span className={"item4"}>
+                  <form action="#">
+                        {/* <input className="searchInput" type="search" name="q" placeholder="Search" /> */}
+                        <button className="searchButton" type="submit">
+                        <i className="material-icons"> search</i>
+                        </button>
+                  </form>
+               
+                <span className={"item3"}>
+                  <ul>
+                    <li>Fon: +49 (421) 696465-0</li>
+                    <li>info@lanthan.eu</li>
+                  </ul>
+                </span>  
+                </span>
               </div>
               
             
